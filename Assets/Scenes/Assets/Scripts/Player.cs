e31f365253d5a91b719ebacef67d093d852460de
Lucas Henrique Procopio <procmode.dev@gmail.com>
2020-06-05 13:38:24 -0300

    :ok_hand: Initial commit

---
+++ Assets/Scenes/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  private Vector2 targetPos;
+  public float yIncrement = 5;
+  public float speed = 50;
+  public int maxHeight = 5;
+  public int minHeight = 0;
+
+  private void Awake()
+  {
+    speed = 50.0f;
+  }
+  // Update is called once per frame
+  void Update()
+  {
+
+    transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+    if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+    {
+      targetPos = new Vector2(transform.position.x, transform.position.y + yIncrement);
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+    {
+      targetPos = new Vector2(transform.position.x, transform.position.y - yIncrement);
+    }
+  }
+}
